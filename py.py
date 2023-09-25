a16ef30c1f9e0e429a7157d5e74704821adb9aa9
Анна Махова <small_storm@mail.ru>
2023-09-25 19:04:01 +0300

    coma

---
+++ py.py
@@ -0,0 +1,18 @@
+# from telnetlib import Telnet
+import telnetlib
+telnet = telnetlib.Telnet('172.17.9.151',4009,15)
+
+def to_bytes(line):
+    return f"{line}\n".encode("utf-8")
+
+with open('conf.txt') as f:
+    for line in f.readlines():
+        telnet.write(to_bytes(line))
+        telnet.write(b"\n")
+        # print(telnet.read_until(b'exit'))
+f.close()
+
+print(telnet.read_until(b'#'))
+print(telnet.read_until(b'#'))
+print(telnet.read_until(b'#'))
+telnet.close()
